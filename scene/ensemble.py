both directions of morphing implemented, correct normalization

scene/ensemble.py
@@ -31,6 +31,15 @@ class Ensemble:
         """
         if self.idx < len(self.filelist['files']) - 1:
             self.idx += 1
+        return self.compute_data
+
+    def decrement(self):
+        """
+        Decrease the index of the currently displayed model.
+        """
+        if self.idx > 0:
+            self.idx -= 1
+        return self.compute_data
 
     def get_num_points(self):
         """
@@ -43,10 +52,9 @@ class Ensemble:
         Create a model for each file in the filelist.
         """
         # mean is used to normalize all point clouds to the same origin and scale
-        mean = None
         for file in self.filelist['files']:
             model = Model(file, self.conf)
-            mean = model.build(mean)
+            model.build()
             self.models.append(model)
 
             self.num_points.append(model.num_points)
@@ -74,27 +82,25 @@ class Ensemble:
         Get the compute data as needed for the compute shader.
         """
         # current points
-
-        oct = self.models[0].octree
-
-        positions = oct.revoke_normalization(oct.points).detach().cpu().numpy()
-        colors = oct.colors
-
-        positions[:,[1, 2]] = positions[:,[2, 1]]
-        positions = np.c_[positions, np.ones(positions.shape[0])]
-
-        compute_data = np.empty((positions.shape[0] + colors.shape[0], 4), dtype="f4")
-        compute_data[0::2,:] = positions
-        compute_data[1::2,:] = colors
+        # if idx is zero, first assignment and the points are from the reference model
+        if self.idx == 0:
+            oct = self.models[0].octree
+            positions = oct.points.detach().cpu().numpy()
+            colors = oct.colors
+            positions = np.c_[positions, np.ones(positions.shape[0])]
+            compute_data = np.empty((positions.shape[0] + colors.shape[0], 4), dtype="f4")
+            compute_data[0::2,:] = positions
+            compute_data[1::2,:] = colors
+        # else we take the points from the first assignment 
+        else:
+            positions = self.correspondences[self.idx - 1]
+            compute_data = np.empty((len(positions) * 2, 4), dtype="f4")
+            compute_data[0::2,:] = np.c_[positions, np.ones(positions.shape[0])]
+            compute_data[1::2,:] = self.matching_colors[self.idx - 1]
+            compute_data = compute_data.astype("f4")
         
         # next points
-
         assignment_positions = self.correspondences[self.idx]
-        # swap columns due to blender
-        assignment_positions[:,[1, 2]] = assignment_positions[:,[2, 1]]
-        # todo 
-        #positions = octrees[i].points
-        #positions[:,[1, 2]] = positions[:,[2, 1]]
         assignment_distances = np.linalg.norm(assignment_positions - positions[:, :3], axis=1)
         # todo
         max_distance = np.max(assignment_distances)
@@ -103,7 +109,6 @@ class Ensemble:
         assignment = np.empty((len(assignment_positions) * 2, 4), dtype="f4")
         assignment[0::2,:] = np.c_[assignment_positions, assignment_distances]
         assignment[1::2,:] = self.matching_colors[self.idx]
-        #assignment[1::2,:] = colors
         assignment = assignment.astype("f4")
 
         return compute_data, assignment

scene/model.py
@@ -14,18 +14,26 @@ class Model:
         self.octree = None
         self.kdtree = None
 
-    def build(self, mean=None):
+    def build(self):
         
         points, colors = file_import.read(self.file)
 
+        # swap columns due to blender
+        points[:,[1, 2]] = points[:,[2, 1]]
+
         self.num_points = points.shape[0]
 
-        # mean is used to normalize all point clouds to the same origin and scale
-        if mean is None:
-            mean = np.mean(points, axis=0)
-        points -= mean
-        normalized_points = points/np.max(np.abs(points))
+        # Step 1: Compute the bounding box
+        min_coords = points.min(axis=0)
+        max_coords = points.max(axis=0)
 
-        self.octree = Octree(normalized_points, self.conf["octree_node_size"], colors=colors, normalize=self.conf["normalize_data"], autograd=self.conf["autograd"])
+        # Step 2: Translate points to center at origin
+        midpoint = (min_coords + max_coords) / 2
+        points_centered = points - midpoint
 
-        return mean 
+        # Step 3: Normalize to range [0, 1]
+        scale = max(max_coords - min_coords)
+        points_normalized = points_centered / scale
+
+        self.octree = Octree(points_normalized, self.conf["octree_node_size"], colors=colors, autograd=self.conf["autograd"])
+    

scene/octree.py
@@ -8,7 +8,7 @@ import torch
 
 class Octree:
 
-    def __init__(self, points, leaf_points, colors=None, normalize=True, autograd=False):
+    def __init__(self, points, leaf_points, colors=None, autograd=False):
 
         self.num_leaf_points = leaf_points # stopage criterion
 
@@ -17,18 +17,10 @@ class Octree:
         self.non_empty_leaf_count = 0
         self.knot_count = 0
         self.point_count = len(points)
-        self.normalize = normalize
 
         self.requires_grad = autograd
 
         self.colors = colors
-
-        if self.normalize:
-            self.min_vals = points.min(axis=0)
-            self.max_vals = points.max(axis=0)
-
-            # Normalize the coordinates
-            points = (points - self.min_vals) / (self.max_vals - self.min_vals)
 
         timer = Timer("Octree Creation:")
         bounds = np.array([[np.min(points[:, 0]) - 0.01, np.min(points[:, 1]) - 0.01, np.min(points[:, 2]) - 0.01],
@@ -86,13 +78,6 @@ class Octree:
         # hierarchy numpy array with row for each knot, indicating the children in self.metadata
         
         return self.hierarchy, self.bounds, self.metadata, self.points, self.colors, self.weights
-
-    def revoke_normalization(self, points):
-        if self.normalize:
-            min_vals = torch.tensor(self.min_vals, dtype=torch.float32, device='cuda')
-            max_vals = torch.tensor(self.max_vals, dtype=torch.float32, device='cuda')
-            return points * (max_vals - min_vals) + min_vals
-        return points
 
 class Node:
     def __init__(self, bounds, nodeindex, level, octree):